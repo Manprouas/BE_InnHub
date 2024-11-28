6c7915f95b67c9f05331376f011b9962a338f153
Faishal Fernando Hutama <faishalfernandohutama@192.168.1.17>
2024-11-28 13:56:44 +0700

    penambahan controller,route hotels

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const route = express.Router();
 const userRoute = require('./user'); 
 const orderRoute = require('./oder.routes')
 const roomRoute = require('./room.routes')
+const hotelRoute = require('./hotel.router')
 
 route.get('/', (req, res) => {
     res.json("express mongoose halo");
@@ -11,5 +12,6 @@ route.get('/', (req, res) => {
 route.use('/users', userRoute);
 route.use('/orders', orderRoute)
 route.use('/rooms', roomRoute)
+route.use('/hotels', hotelRoute)
 
 module.exports = route;
